160aa663f7ee6d0f9dcd3ead5907dd46bad12f4a
ivensgustavo <gustavo_ivens@alu.ufc.br>
2020-08-24 11:23:43 -0300

    Remoção da pasta com erros atividade_05

--- atividade_05/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const routes = express.Router()
-const disciplinaRouter = require('../routes/disciplina.routes');
-
-
-routes.use('/disciplinas', disciplinaRouter);
-module.exports = routes;
